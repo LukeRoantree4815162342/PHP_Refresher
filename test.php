ceedb93ec204fe9918e5c766d0fea328f13aa8c3
LUKEPC\Luke <allhailluke@roantree.com>
2017-04-28 20:39:40 +0100

    json now playing nice

--- test.php
+++ test.php
@@ -10,11 +10,22 @@ function addRecordTestTable($name, $age){
   $stmt->bindParam(':age', $age);
   $stmt->execute();
 }
+?>
+<head>
+  <script src="js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+enter lat and long:<br>
+<input id="inlat" type="text">
+<input id="inlong" type="text">
+<button id="inbtn" onclick="go();">set lat / long</button>
 
 
+
+<?php
 print "<table border=1>";
 print "<tr><td>Name</td><td>Age</td></tr>";
-addRecordTestTable("paul'; DROP TABLES *", 39);
+//addRecordTestTable("paul'; DROP TABLES *", 39);
 $result = $db->query('SELECT * FROM testtable');
 foreach($result as $row)
 {
@@ -22,8 +33,34 @@ foreach($result as $row)
   print "<td>".$row['age']."</td></tr>";
 }
 
+
+
 print "</table>";
+?>
+<ul id="data"></ul>
+<script type="text/javascript">
+  function go() {
+    lat = $("#inlat").val();
+    long = $("#inlong").val();
+    urlpolice = "https://data.police.uk/api/crimes-street/all-crime?lat="+lat+"&lng="+long+"&date=2013-01";
+    $.get(urlpolice, function (data) {
+      data = JSON.parse(data);
+      var streets = ['null'];
+      var iter = 0;
+      data.forEach(function(entry) {
+        streets[iter] = "<li> " + entry.category + " </li>";
+        iter+=1;
+      });
+      $("#data").html(streets);
+    }, dataType="text");
+  }
+</script>
+</body>
+<?php
+
 // close the database connection
 $db = NULL;
+
+
 
 
